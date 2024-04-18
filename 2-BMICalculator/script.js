ec00b8a4d91e651e6c215f22f39da80eb2eafa45
upriser72 <upriser72@gmail.com>
2024-04-19 01:29:30 +0530

    k

--- 2-BMICalculator/script.js
+++ 2-BMICalculator/script.js
@@ -1,22 +1,25 @@
-const form = document.querySelector('form');
+const form = document.querySelector('form')
 // this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    //on calculate button,by default input values of ht and wt 
+    //will get submitted so prevent that
 
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results').value
 
-  if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
-  } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
-  }
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = 'Please give a valid height';
+      } 
+      else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = 'Please give a valid weight';
+      } 
+      else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //show the result
+        results.innerHTML = `<span>${bmi}</span>`;
+      }
 });
-

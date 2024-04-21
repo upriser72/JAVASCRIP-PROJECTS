5901b230538cc82f7a588a5d1665f93a68539489
upriser72 <upriser72@gmail.com>
2024-04-21 18:21:53 +0530

    BMI

--- 2-BMICalculator/script.js
+++ 2-BMICalculator/script.js
@@ -10,14 +10,32 @@ form.addEventListener('submit', function (e) {
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
 
-  if (height === '' || height < 0 || isNaN(height)) {
+  if (height === '' || height < 0 || isNaN(height)) 
+  {
     results.innerHTML = "Please give a valid height";
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+  } 
+  else if (weight === '' || weight < 0 || isNaN(weight)) 
+  {
     results.innerHTML = "Please give a valid weight";
-  } else {
+  } 
+  else 
+  {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    // results.innerHTML = `<span>${bmi}</span><br>`;
+    if(bmi<18.6)
+    {
+      results.innerHTML = `<span>${bmi}<br>You are Underweight</span>`;
+    }
+    else if(bmi>=18.6 && bmi<=24.9)
+    {
+      results.innerHTML = `<span>${bmi}<br>You have a Healthy BMI</span>`;
+    }
+    if(bmi>24.9)
+    {
+      results.innerHTML = `<span>${bmi}<br>You are Overweight</span>`;
+    }
   }
+
 });
 
